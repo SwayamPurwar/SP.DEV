14d710f7316959ae701d2b418da30c6e626e0602
Swayam Purwar <swayampurwar111104@gmail.com>
2026-03-23 11:51:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import PageTransition from "./components/PageTransition";
@@ -103,7 +102,6 @@ function App() {
       </Suspense>
 
       <Footer />
-       <Analytics />
     </>
   );
 }
